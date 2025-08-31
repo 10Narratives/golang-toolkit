57789b9655dd55469e432a81215e1e8fbbd92581
10Narratives <vanyatertyshnikov@yandex.ru>
2025-08-31 12:37:09 +0300

    fix logging lib

--- logging/config.go
+++ logging/config.go
@@ -1,7 +1,7 @@
 package logging
 
 type LoggingConfig struct {
-	Level  string `yaml:"level" env-default:"8"`
+	Level  int    `yaml:"level" env-default:"8"`
 	Format string `yaml:"format" env-default:"json"`
 	Output string `yaml:"output" env-default:"stdout"`
 }
